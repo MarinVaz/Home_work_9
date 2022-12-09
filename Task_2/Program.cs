fa64bfab212df66d4f1668564dc7211c37b95ae9
Marina <errorda@gmail.com>
2022-12-09 12:42:51 +0300

    added task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int columns = ReadInt("Введите количество столбцов матрицы: ");
+int[,] numbers = new int[rows, columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+MinSumElements(numbers);
+Console.WriteLine();
+
+void MinSumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < numbers.GetLength(1); i++)
+    {
+        minRow += numbers[0, i];
+    }
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++) 
+        sumRow += numbers[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+for(int i = 0; i < array.GetLength(0); i++)
+{
+for(int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = new Random().Next(0,10);
+}
+}
+}
+
+void WriteMatrix(int[,] array)
+{
+for(int i = 0; i < array.GetLength(0); i++)
+{
+for(int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write(array[i, j] + " ");
+}
+Console.WriteLine();
+}
+Console.WriteLine();
+}
